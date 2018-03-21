acba135683fca9fa57ddb83c953a878998899cf4
gomisha <misha@gomisha.com>
2018-03-21 13:29:17 -0400

    initial example

---
+++ src/ReadFile_BufferedInputStream.java
@@ -0,0 +1,30 @@
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class ReadFile_BufferedInputStream {
+	public static void main(String [] pArgs) throws FileNotFoundException, IOException {
+		System.out.println("Hello ReadFile_BufferedInputStream");
+		System.out.println("javaVersion=" + System.getProperty("java.version"));
+
+		String fileName = "sample1.txt";
+		File file = new File(fileName);
+		FileInputStream fileInputStream = new FileInputStream(file);
+		
+		try (BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream)) {
+			long startTime = System.currentTimeMillis();
+
+			int singleCharInt;
+			char singleChar;
+			while((singleCharInt = bufferedInputStream.read()) != -1) {
+				singleChar = (char) singleCharInt;
+				//System.out.println(singleChar);
+			}
+			long stopTime = System.currentTimeMillis();
+			long elapsedTime = stopTime - startTime;
+			System.out.println("elapsedTime=" + elapsedTime);
+		}
+	}
+}
