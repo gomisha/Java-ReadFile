823490cf6d06b1be7ee83e6f5ef98fd820683ddd
gomisha <misha@gomisha.com>
2018-03-23 14:03:47 -0400

    added encoding example

--- src/ReadFile_Scanner_NextLine_Encoding.java
+++ src/ReadFile_Scanner_NextLine_Encoding.java
@@ -2,14 +2,15 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class ReadFile_Scanner_NextLine {
+public class ReadFile_Scanner_NextLine_Encoding {
 	public static void main(String [] pArgs) throws FileNotFoundException {
 		System.out.println("javaVersion=" + System.getProperty("java.version"));
 
 		String fileName = "c:\\temp\\2.sample-10KB.txt";
 		File file = new File(fileName);
 		
-		try (Scanner scanner = new Scanner(file)) {
+		//use UTF-8 encoding
+		try (Scanner scanner = new Scanner(file, "UTF-8")) {
 			long startTime = System.currentTimeMillis();
 			String line;
 			boolean hasNextLine = false;
