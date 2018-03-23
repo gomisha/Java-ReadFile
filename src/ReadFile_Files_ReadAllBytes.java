e07cccca385c9091e33ba8191fe3d52bd568cfd7
gomisha <misha@gomisha.com>
2018-03-23 14:12:51 -0400

    minor edits before article submission

--- src/ReadFile_Files_ReadAllBytes.java
+++ src/ReadFile_Files_ReadAllBytes.java
@@ -6,16 +6,16 @@ public class ReadFile_Files_ReadAllBytes {
 	public static void main(String [] pArgs) throws IOException {
 		System.out.println("javaVersion=" + System.getProperty("java.version"));
 
-		String fileName = "sample1.txt";
+		String fileName = "c:\\temp\\2.sample-10KB.txt";
 		File file = new File(fileName);
 
 		long startTime = System.currentTimeMillis();
 		byte [] fileBytes = Files.readAllBytes(file.toPath());
 		long stopTime = System.currentTimeMillis();
 
-//		for(byte b : fileBytes) {
-//			System.out.println((char) b);
-//		}
+		for(byte b : fileBytes) {
+			System.out.print((char) b);
+		}
 
 		long elapsedTime = stopTime - startTime;
 		System.out.println("elapsedTime=" + elapsedTime);
