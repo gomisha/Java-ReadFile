db35568bd23ab2adbb6e59b527a7554908a27c40
gomisha <misha@gomisha.com>
2018-03-23 13:53:26 -0400

    added encoding example

--- src/ReadFile_Files_Lines_Encoding.java
+++ src/ReadFile_Files_Lines_Encoding.java
@@ -1,9 +1,10 @@
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.stream.Stream;
 
-public class ReadFile_Files_Lines {
+public class ReadFile_Files_Lines_Encoding {
 	public static void main(String[] pArgs) throws IOException {
 		System.out.println("javaVersion=" + System.getProperty("java.version"));
 
@@ -11,13 +12,13 @@ public class ReadFile_Files_Lines {
 		File file = new File(fileName);
 
 		long startTime = System.currentTimeMillis();
-		
-		try (Stream<String> linesStream = Files.lines(file.toPath())) {
+
+		try (Stream<String> linesStream = Files.lines(file.toPath(), StandardCharsets.UTF_8)) {
 			linesStream.forEach(line -> {
 				System.out.println(line);
 			});
 		}
-		
+
 		long stopTime = System.currentTimeMillis();
 		long elapsedTime = stopTime - startTime;
 		System.out.println("elapsedTime=" + elapsedTime);
